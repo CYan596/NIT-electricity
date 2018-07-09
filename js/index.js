6d811112fe79da41a388930bf0435cd50d7260e3
cyan596 <cyan.zhukeqing@qq.com>
2018-07-09 20:03:16 +0800

    1
    1

--- js/index.js
+++ js/index.js
@@ -8,15 +8,25 @@ console.log(barArr)
 // B tab栏点击切换
 
 barArr[0].onclick = function () {
-  $(pageArr[0]).addClass('top').siblings().removeClass('top')
+  switchScreen(0)
+  // $(pageArr[0]).addClass('top').siblings().removeClass('top')
 }
 
 barArr[1].onclick = function () {
-  $(pageArr[1]).addClass('top').siblings().removeClass('top')
+  switchScreen(1)
+  // $(pageArr[1]).addClass('top').siblings().removeClass('top')
 }
 
 barArr[2].onclick = function () {
-  $(pageArr[2]).addClass('top').siblings().removeClass('top')
+  switchScreen(2)
+  // $(pageArr[2]).addClass('top').siblings().removeClass('top')
 }
 
 // E tab栏点击切换
+
+function switchScreen (i) {
+  $(pageArr[i]).addClass('top').siblings().removeClass('top')
+  $(barArr[i]).addClass('active').siblings().removeClass('active')
+}
+
+
