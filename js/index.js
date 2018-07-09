31b2d984ac24a7e352f7cb4e76e76afb1c9f596b
cyan596 <cyan.zhukeqing@qq.com>
2018-07-09 20:06:08 +0800

    2
    2

--- js/index.js
+++ js/index.js
@@ -9,17 +9,14 @@ console.log(barArr)
 
 barArr[0].onclick = function () {
   switchScreen(0)
-  // $(pageArr[0]).addClass('top').siblings().removeClass('top')
 }
 
 barArr[1].onclick = function () {
   switchScreen(1)
-  // $(pageArr[1]).addClass('top').siblings().removeClass('top')
 }
 
 barArr[2].onclick = function () {
   switchScreen(2)
-  // $(pageArr[2]).addClass('top').siblings().removeClass('top')
 }
 
 // E tab栏点击切换
