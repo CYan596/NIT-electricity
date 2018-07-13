fb0754ad914d39cbf65e4816680d8e767cbe970a
cyan596 <cyan.zhukeqing@qq.com>
2018-07-13 20:45:24 +0800

    38
    38

--- js/index.js
+++ js/index.js
@@ -71,15 +71,18 @@ function render (i) {
 
 // B 寝室选择
 // 寝室选择选项数组生成
+// 本地Picker数据初始化
+
+
 var data1 = []
 for (var i = 1; i <= 15; i++) {
   data1.push({
     text: i + '栋' + 'A',
-    value: 1
+    value: i
   })
   data1.push({
     text: i + '栋' + 'B',
-    value: 1
+    value: i + 'B'
   })
 }
 var data2 = []
@@ -95,12 +98,12 @@ for (var i = 1; i <= 29; i++) {
   if (i < 10) {
     data3.push({
       text: '0' + i + '寝',
-      value: 1
+      value: '0' + i
     })
   } else {
     data3.push({
       text: i + '寝',
-      value: 1
+      value: i
     })
   }
 }
@@ -109,10 +112,13 @@ for (var i = 1; i <= 29; i++) {
 // var nameEl = document.getElementById('select')
 // var nameEl = $('#select')[0]
 var nameEl = selectArr.get(0)
+var pickerIndex = []
+localPickerInit()
+
 
 var picker = new Picker({
   data: [data1, data2, data3],
-  selectedIndex: [0, 0, 0],
+  selectedIndex: pickerIndex,
   title: '选择寝室'
 });
 
@@ -123,16 +129,43 @@ picker.on('picker.select', function (selectedVal, selectedIndex) {
 picker.on('picker.change', function (index, selectedIndex) {
   // console.log(index);
   // console.log(selectedIndex);
+  // loadingActive()
 });
 
 picker.on('picker.valuechange', function (selectedVal, selectedIndex) {
   // console.log(selectedVal);
   // console.log(selectedIndex);
+  loadingActive()
+  // console.log(selectedIndex)
+  var str1 = JSON.stringify(selectedIndex);   
+  localStorage.setItem("localPickerIndex", str1); 
 });
 
 nameEl.addEventListener('click', function () {
   picker.show();
 });
+
+// 本地数据读取
+var localPickerIndex
+function localPickerInit () {
+  // 1.读取本地localPikerData
+  // localPickerIndex = parseInt(localStorage.getItem("localPickerIndex"))
+  if(localStorage.getItem("localPickerIndex") == null) {
+    console.log('无用户数据缓存')
+    pickerIndex = [0, 0, 0]
+  }
+  else{
+      console.log('有用户数据缓存')
+  // 2.若有，直接替换PickerIndex
+      var str2 = localStorage.getItem("localPickerIndex");
+      // console.log(str2)
+      // pickerIndex = JSON.parse(str2))
+      pickerIndex = JSON.parse(str2);
+      nameEl.innerText = data1[pickerIndex[0]].text + ' ' + data2[pickerIndex[1]].text + ' ' + data3[pickerIndex[2]].text;
+  }
+
+  // 3、若无，用户选择后存储到本地
+}
 
 // E 寝室选择
 
