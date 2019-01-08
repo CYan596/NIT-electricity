ac284458c56a52a0b8bf0d82cef4ba50eba20cc5
cyan596 <cyan.zhukeqing@qq.com>
2019-01-08 22:20:00 +0800

    145
    145

--- APItest/src/components/about.vue
+++ APItest/src/components/about.vue
@@ -1,8 +1,20 @@
 <template>
 	<div class="body">
-		关于，界面
+		<div class="card flex-center" id="board-card">
+			<div>
+				<p>显然，我们需要一套好看的图标</p>
+				<p>你愿意帮助我们吗</p>
+			</div>
+		</div>
+		<mt-cell title="此项目的开发日志" label="将跳转到知乎 · 更新与开发日志" is-link
+		to="https://www.zhihu.com/people/zhu-ke-qing-90/activities"
+		></mt-cell>
+		<mt-cell title="建议与意见" label="将跳转到知乎 · 建议与意见（请在评论区留言）" is-link
+		to="https://www.zhihu.com/people/zhu-ke-qing-90/activities"
+		></mt-cell>
+
 	</div>
-</template>   
+</template>
 
 <script>
 	export default{
@@ -19,8 +31,36 @@
 
 <style scoped>
 .body{
-	color: #fff;
+	/* color: #fff;
 	background-color: green;
-	height: 50px;
+	height: 50px; */
+	padding: 0 10px;
+}
+.card{
+	color: #fff;
+	padding: 10px;
+	margin: 10px 0 0 0;
+	border: 1px solid #dbdbdb;
+	border-radius: 6px;
+	box-shadow: 4px 4px 6px rgba(0,0,0,.1);
+}
+.card .card-font-dark{
+	font-weight: bold;
+	font-size: 18px;
+	/* text-shadow: 0px 0px 6px rgba(0,0,0,.1); */
+}
+.card .card-font-light{
+	font-weight: 100;
+	font-size: 12px;
+}
+#board-card{
+	height: 110px;
+	margin-bottom: 10px;
+	background-color: #5d85dc;
+}
+.flex-center{
+	display: flex;
+	justify-content: center;
+	align-items: center;
 }
 </style>
